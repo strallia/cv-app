9394916a2fb64c5466c8a56d0174c118afcc6fb7
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-04-07 22:37:54 -0700

    feat: Create template for data

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const data = {
+  person: [
+    {
+      firstName: "first",
+      lastName: "last",
+      phone: "phone",
+      email: "email",
+    },
+  ],
+  education: [
+    {
+      degree: "degree",
+      school: "school",
+      startYear: "start",
+      endYear: "end",
+      description: "descrip",
+    },
+    {
+      degree: "degree",
+      school: "school",
+      startYear: "start",
+      endYear: "end",
+      description: "descrip",
+    },
+  ],
+  experience: [
+    {
+      title: "title",
+      company: "company",
+      startYear: "start",
+      endYear: "end",
+      description: "descrip",
+    },
+    {
+      title: "title",
+      company: "company",
+      startYear: "start",
+      endYear: "end",
+      description: "descrip",
+    },
+  ],
+};
